348d2815603eb366100a7166cbe85b66674bc6f0
ulysses316 <ulises316@live.com.mx>
2020-08-29 07:33:00 -0500

    Taller

--- controler.js
+++ controler.js
@@ -1,22 +1,21 @@
 const baseURL = ()=>{
-    let arr = [];
+    let arrValue = [];
     let parametro = document.getElementById("pokemon").value;
     let url = 'https://pokeapi.co/api/v2/pokemon/' + parametro;
     fetch(url)
         .then((response => response.json()))
         .then(myJson => {
+            let element = document.getElementById('poke-table');
+            element.style.display = "block";
             document.getElementById("image").src = myJson.sprites.front_default
-            arr.push(myJson.name);
-            arr.push(myJson.id);
-            arr.push(myJson.base_experience);
-            arr.push(myJson.height);
-            for (index in arr) {
-                param = document.getElementById("parameter" + index).innerHTML = arr[index];
-                console.log(`parameter${index} + : arr[${index}]`);
-                console.log()
+            arrValue.push(myJson.name);
+            arrValue.push(myJson.id);
+            arrValue.push(myJson.base_experience);
+            arrValue.push(myJson.height);
+            for (index in arrValue) {
+                document.getElementById("parameterValue" + index).innerHTML = arrValue[index];
             }
         })
-    return arr
 }
 
 const typeURL = ()=>{
