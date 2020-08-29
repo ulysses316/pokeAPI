42508ebc06ce35edebc24b34075a7808228ae51a
ulysses316 <ulises316@live.com.mx>
2020-08-29 02:26:43 -0500

    second backup

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-baseURL("pikachu")
+const baseURLMenu = ()=>{
+  let element = document.getElementById('pokemonOrNumber');
+  element.style.display = "block";
+}
+
+const typeURLMenu = ()=>{
+  let element = document.getElementById('numberType');
+  element.style.display = "block";
+}
+
+const abilityURLMenu = ()=>{
+  let element = document.getElementById('numberAbility');
+  element.style.display = "block";
+}
--- routes.js
+++ routes.js
@@ -1,18 +1,22 @@
-const baseURL = (numberOrPokemon)=>{
+const baseURL = ()=>{
+    let parametro = document.getElementById("pokemon");
     let url = 'https://pokeapi.co/api/v2/pokemon/' + numberOrPokemon;
     fetch(url)
         .then((response => response.json()))
         .then(myJson => console.log(myJson))
 }
 
-const typeURL = (numberType)=>{
+const typeURL = ()=>{
+    let parametro = document.getElementById("type");
     let url = 'https://pokeapi.co/api/v2/pokemon/' + numberType;
     fetch(url)
     .then((response => response.json()))
     .then(myJson => console.log(myJson))
 }
 
-const abilityURL = (numberAbility)=>{
+const abilityURL = ()=>{
+    let parametro = document.getElementById("ability").value;
+    console.log(parametro)
     let url = 'https://pokeapi.co/api/v2/pokemon/' + numberAbility;
     fetch(url)
     .then((response => response.json()))
